cece59c08fb43400b1360d7abf7fa1eddec45a22
Harish Shetty <harish@workato.com>
2017-03-21 05:12:20 +0000

    Update docs

--- publish_gitbook.sh
+++ publish_gitbook.sh
@@ -1,6 +1,5 @@
 # https://gist.githubusercontent.com/SangsooNam/aa73c3e1ff88d30433e4020f1275242a/raw/b5fdc4d1cc44be63dc272a42b55524a1cf32d595/publish_gitbook.sh
 # install the plugins and build the static site
-
 gitbook install && gitbook build
 
 # checkout to the gh-pages branch
