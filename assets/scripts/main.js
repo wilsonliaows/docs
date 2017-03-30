160a0bccae367d014a43c358867ed335095cfacf
th0r <grunin.ya@ya.ru>
2017-03-30 16:05:32 +0300

    Add anchor links to markdown headings

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,17 +1,38 @@
 require(['gitbook', 'jquery'], function (gitbook, $) {
-  gitbook.events.on('start', function () {
-    // Removing "Menu" button
-    gitbook.toolbar.removeButton('btn-0');
+  gitbook.events.on('page.change', function () {
+    // Should do it on `page.change` because buttons are being redrawn on every page change for some reason
+    updateToolbarButtons();
 
-    // Adding custom "Menu" button
-    gitbook.toolbar.createButton({
-      index: 0,
-      text: 'Menu',
-      icon: 'fa fa-align-justify',
-      onClick: function (e) {
-        e.preventDefault();
-        gitbook.sidebar.toggle();
-      }
-    });
+    // Adding anchor icons next to markdown headers
+    addHeaderLinks();
   });
+
+  function updateToolbarButtons() {
+    // Adding title to `Menu` button
+    $('.book-header .btn:has(.fa-align-justify)')
+      .append('<span> Menu</span>');
+
+    // Changing icon for `Edit This Page` button
+    $('.book-header .btn .fa-edit')
+      .removeClass('fa-edit')
+      .addClass('fa-github');
+  }
+
+  function addHeaderLinks() {
+    $('.markdown-section')
+      .find('h1, h2, h3, h4, h5')
+      .filter(function () {
+        return this.getAttribute("id");
+      })
+      .addClass('heading heading_linkable')
+      .prepend(function () {
+        return (
+          '<a class="heading__anchor" ' +
+            'href="#' + this.getAttribute('id') + '">' +
+            '<i class="fa fa-link"></i>' +
+          '</a>'
+        );
+      });
+
+  }
 });
